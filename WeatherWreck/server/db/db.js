5a99397580b44051784f449fa830f6fa37d93f8f
Youry Nelson <yourymorgannelson@gmail.com>
2024-10-26 12:53:37 -0400

    Create db class, add constructor

--- WeatherWreck/server/db/db.js
+++ WeatherWreck/server/db/db.js
@@ -2,3 +2,25 @@ import 'dotenv/config';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const dburl = process.env.ATLAS_URI;
+
+let instance = null;
+
+class DB{
+  //Connecting the DB
+  constructor(){
+    if(!instance){
+      instance = this;
+      this.client = new MongoClient(dburl, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        }
+      }
+      );
+      this.db = null;
+      this.collection = null;
+    }
+    return instance;
+  }
+}
