c713265a8baa0e5428d8a42d43f401b85ad07bb4
Youry Nelson <yourymorgannelson@gmail.com>
2024-10-27 16:09:05 -0400

    Create function to populate db with data from csv

--- WeatherWreck/server/util/seed.js
+++ WeatherWreck/server/util/seed.js
@@ -23,3 +23,31 @@ const data = collections.map((coll, index) =>  ({
   'name': coll,
   'filePath': csvFiles[index]
 }));
+
+(async () =>{
+  try{
+    await db.connect(dbName);
+    await Promise.all(data.map(collection => db.open(collection['name'])));
+
+    await Promise.all(data.map(async(collection)=>{
+      const dataToInsert = [];
+      const fileContent = await fs.readFile(collection['filePath'], 'utf-8');
+
+      await new Promise((resolve, reject) => {
+        parse(fileContent, { 
+          columns: true,
+          
+        }).
+          on('data', (row) => dataToInsert.push(row)).
+          on('end', resolve).
+          on('error', reject); 
+      });
+      await db.createMany(collection['name'], dataToInsert);
+    }));
+    await db.close();
+    console.log('Database seeding completed.');
+  }catch(e){
+    console.error('Error seeding the database:', e);
+    await db.close();
+  }
+})();
