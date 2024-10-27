a1acf437ba0aa160f4bafd5ce6f32a9995234f69
Youry Nelson <yourymorgannelson@gmail.com>
2024-10-27 16:07:34 -0400

    Create variables for info on db and data to add

--- WeatherWreck/server/util/seed.js
+++ WeatherWreck/server/util/seed.js
@@ -3,3 +3,7 @@ import fs from 'fs/promises';
 import path from 'path';
 import { parse } from 'csv-parse';
 import { db } from '../db/db.js';
+
+const folderPath = path.join('./db', '/mockData');
+const dbName = 'WeatherWreck';
+const collections = ['CarAccidents', 'WeatherForecast'];
