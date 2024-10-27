bfc9b6668afce4f5436514ff74dcff1531f147cd
Youry Nelson <yourymorgannelson@gmail.com>
2024-10-27 16:07:57 -0400

    create function to read csv files from a folder

--- WeatherWreck/server/util/seed.js
+++ WeatherWreck/server/util/seed.js
@@ -7,3 +7,13 @@ import { db } from '../db/db.js';
 const folderPath = path.join('./db', '/mockData');
 const dbName = 'WeatherWreck';
 const collections = ['CarAccidents', 'WeatherForecast'];
+
+async function getFilePaths(folderPath) {
+  try{
+    const data = await fs.readdir(folderPath);
+    return data.map(file => path.join(folderPath, file));
+  }catch(e){
+    console.error(e);
+    return [];
+  }
+};
