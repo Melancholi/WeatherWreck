332879594035a8d679b9aea0da1300f92f830e4e
Iana Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-10-28 10:59:37 -0400

    Initilaize the main server api.mjs.

--- WeatherWreck/server/api.mjs
+++ WeatherWreck/server/api.mjs
@@ -1,12 +1,14 @@
 import express from 'express';
+import accidentRouter from './routers/accidents.js';
+import weatherRouter from './routers/weather.js';
 
 
 const app = express();
-const port = 3000;
-
 
 //Serve the static files from the React app
 app.use(express.static('./../client/dist'));
+app.use('/api/accidents', accidentRouter);
+app.use('/api/weather', weatherRouter);
 
 //When data initialized start listening
 app.listen(port, () => {
---
+++ WeatherWreck/server/controllers/accidentController.js
@@ -0,0 +1,12 @@
+export function getAllAccidents(req, res){
+  //Logic
+}
+export function getAccidentsByState(req, res){
+  //Logic
+}
+export function getAccidentsByDate(req, res){
+  //Logic
+}
+export function getAccidentsBySeverity(req, res){
+  //Logic
+}
