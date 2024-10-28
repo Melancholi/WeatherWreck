1806652125d99a9447902c0877c2c8f6ac9faf15
Iana Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-10-28 11:16:08 -0400

    Implement API endpoints structure.

---
+++ WeatherWreck/server/controllers/weatherController.js
@@ -0,0 +1,12 @@
+export function getWeatherEvents(req,res){
+  //Logic
+}
+export function getWeathereventsByState(req,res){
+  //Logic
+}
+export function getWeatherEventsByDate(req,res){
+  //Logic
+}
+export function getWeathereventsByType(req,res){
+  //Logic
+}
