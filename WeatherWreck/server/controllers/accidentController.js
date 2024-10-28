c40ba51c63457efef3330f142312a1ce90a2e0d8
Iana Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-10-28 18:58:09 -0400

    Finish creating endpoints.

--- WeatherWreck/server/controllers/accidentController.js
+++ WeatherWreck/server/controllers/accidentController.js
@@ -1,12 +1,25 @@
-export function getAccidents(req, res){
-  //Logic
+import {db} from '../db/db.js';
+
+export async function getAccidents(req, res){
+  const accidents = await db.readAll('CarAccidents');
+  res.json( accidents);
 }
-export function getAccidentsByState(req, res){
-  //Logic
+export async function getAccidentsByState(req, res){
+  const state = req.params.state.toLowerCase();
+  //Capitalize first letter
+  const camelCaseState = state[0].toUpperCase() + state.slice(1);
+  const data = await db.readByCondition('CarAccidents', {State:{camelCaseState}})
+  res.json(data);
 }
-export function getAccidentsByDate(req, res){
-  //Logic
+export async function getAccidentsByDate(req, res){
+  const date = req.params.date;
+  const data = await db.readByCondition('CarAccidents', {Date:{date}})
+  res.json(data);
 }
-export function getAccidentsBySeverity(req, res){
-  //Logic
+export async function getAccidentsBySeverity(req, res){
+  const severity = req.params.severity.toLowerCase();
+  //Capitalize first letter
+  const camelCaseType = state[0].toUpperCase() + state.slice(1);
+  const data = await db.readByCondition('CarAccidents', {Type:{camelCaseType}})
+  res.json(data);
 }
--- WeatherWreck/server/controllers/weatherController.js
+++ WeatherWreck/server/controllers/weatherController.js
@@ -1,12 +1,26 @@
-export function getWeatherEvents(req,res){
-  //Logic
+import {db} from '../db/db.js';
+
+export async function getWeatherEvents(req,res){
+  const data = await db.readAll('WeatherForcast');
+  res.json(data);
 }
-export function getWeatherEventsByState(req,res){
-  //Logic
+export async function getWeatherEventsByState(req,res){
+  const state = req.params.state.toLowerCase();
+  //Capitalize first letter
+  const camelCaseState = state[0].toUpperCase() + state.slice(1);
+  const data = await db.readByCondition('WeatherForcast', {State:{camelCaseState}})
+  res.json(data);
+
 }
-export function getWeatherEventsByDate(req,res){
-  //Logic
+export async function getWeatherEventsByDate(req,res){
+  const date = req.params.date;
+  const data = await db.readByCondition('WeatherForcast', {Date:{date}})
+  res.json(data);
 }
-export function getWeatherEventsByType(req,res){
-  //Logic
+export async function getWeatherEventsByType(req,res){
+  const type = req.params.type.toLowerCase();
+  //Capitalize first letter
+  const camelCaseType = await state[0].toUpperCase() + state.slice(1);
+  const data = db.readByCondition('WeatherForcast', {Type:{camelCaseType}})
+  res.json(data);
 }
