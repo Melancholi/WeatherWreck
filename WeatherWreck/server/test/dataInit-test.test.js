34f978f4a5034269ea35b3eed3bcc079d87543a8
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-10-28 11:08:20 -0700

    Got rid of failing test

--- WeatherWreck/server/test/dataInit-test.test.js
+++ WeatherWreck/server/test/dataInit-test.test.js
@@ -22,17 +22,6 @@ const matchedMockAccidentsCsv = './db/mockData/result/matched_mock_accident_data
 const matchedMockWeatherCsv = './db/mockData/result/matched_mock_weather_data.csv';
 
 describe('Data Initialization and Matching Tests', function() {
-  // before(async function() {
-  //   // Cleaing up the previous output before tests
-  //   // Learned about this from here https://nodejs.org/api/fs.html#fspromisesrmpath-options
-  //   try {
-  //     await fs.rm(matchedMockAccidentsCsv, { force: true });
-  //     await fs.rm(matchedMockWeatherCsv, { force: true });
-  //   } catch (error) {
-  //     console.error('Error during cleanup:', error);
-  //   }
-  // });
-
   describe('Checking if even matches', function() {
     it('Should return true if the events match', function() {
       const accident = {
@@ -76,33 +65,6 @@ describe('Data Initialization and Matching Tests', function() {
 
       let dataMatch = isDataMatching(accident, weather); 
       return expect(dataMatch).to.be.false;
-    });
-  });
-
-  describe('Adding matching events', function() {
-    it('It should pass if matches added to the accidents CSV file', async function() {
-      await matchAccidentsWithWeather(trimmedMockAccidents, trimmedMockWeather, matchedMockAccidentsCsv, matchedMockWeatherCsv);
-      const weatherData = await readCsvData(matchedMockWeatherCsv);
-      const accidentData = await readCsvData(matchedMockAccidentsCsv)
-
-      const accident = {
-        ID: 'A-512236', State: 'WA', City: 'Seattle',
-        Severity: 3, Start_Time: '2022-11-20T10:30:00Z',
-        End_Time: '2022-11-20T11:15:00Z', Start_Lat: '47.6062',
-        Start_Lng: '-122.3321', Description: 'Collision on I-5 Northbound near Madison St.',
-        Street: 'I-5 N', End_lat: '', End_Lng: '', 'Distance(mi)': '0.0',
-        Weather_Condition: 'Snow'
-      };
-      const weather = {
-          EventId: 'W-2238', State: 'WA', City: 'Seattle',
-          'StartTime(UTC)': '2022-11-20T10:30:00Z',
-          'EndTime(UTC)': '2022-11-20T11:15:00Z',
-          Severity: 1, Type: 'Snow', LocationLat: '47.6062',
-          LocationLng: '-122.3321', 'Precipitation(in)': '0.0'
-      };
-
-      expect(accidentData).to.include(accident);
-      expect(weatherData).to.include(weather);
     });
   });
 });
