4a69db1045141d5c24ae455a3eaf88c09c63fbb4
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-10-27 18:56:19 -0700

    Change the logic of the code in data-init file

--- WeatherWreck/server/data-init.mjs
+++ WeatherWreck/server/data-init.mjs
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as fsp from 'fs/promises';
 import csv from 'csv-parser';
+import { parse } from 'csv-parser';
 
 // The paths for the input files
 const trimmedAccidentsPath = './db/initialDB/Trimmed_Accidents.csv';
@@ -11,111 +12,227 @@ const matchingAccidentsCsvPath = './db/initialDB/result/Matching_Accidents_CSV.c
 const matchingWeathersCsvPath = './db/initialDB/result/Matching_Weathers_CSV.csv';
 
 /**
+ * This function is used to read the record from a specific file
+ * 
+ * @param {string} csvFile - The path to the weather CSV file 
+ * @returns {Promise} - A promise that resolves to an array of data objects
+ * 
+ * @author Maara Vanessa Purici
+ */
+export async function readCsvData(csvFile) {
+  const data = [];
+  // To do this, I used the examples from here 
+  // https://www.scaler.com/topics/read-csv-javascript/
+  await new Promise((resolve, reject) => {
+    fs.createReadStream(csvFile).
+      pipe(parse({
+        delimiter: ",",
+        columns: true,
+        ltrim: true,
+      })).
+      on('data', (row) => {
+        data.push(row);
+      }).
+      on('error', reject).
+      on('end', resolve);
+  });
+  return data;
+}
+
+/**
  * Helper function that is used to check if an event already exists in 
  * a CSV file based on a specific id
  * 
  * @param {string} filePath - The path of the CSV file to check 
- * @param {string} id - The id of the evnt to search for 
+ * @param {string} id - The id of the event to search for 
  * @returns {boolean} - Returns true if the event id is found, 
  * otherwise false
  * 
  * @author Maara Vanessa Purici
  */
 export async function eventExists(filePath, id) {
+  const data = await readCsvData(filePath);
   let found = false;
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(filePath).
-      pipe(csv()).
-      on('data', (event) => {
-        if (event.ID === id || event.EventId === id) {
-          // Exits early if a match is found
-          found = true;
-        }
-      }).
-      on('end', () => {
-        resolve(found);
-      }).
-      on('error', () => {
-        console.error(`Error:`, error);
-        reject(error);
-      });
+  data.forEach((event) => {
+    if (event.ID === id || event.EventId === id) {
+      // Exits early if a match is found
+      found = true;
+    }
+  });
+  return found;
+}
+
+/**
+ * This function is used to write to the matched accidents CSV file
+ *  
+ * @param {string} accidentCsv - Path to the matching accidents CSV file.
+ * @param {Object} accident - The accident to add to the CSV file.
+ * 
+ * @author Maara Vanessa Purici
+ */
+export function writeToAccidentsCsv(accidentCsv, accident) {
+  // To do this, I used an example form here
+  // https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
+  const csvData = extractAsCsvForAccidents(accident);
+  fs.writeFile(accidentCsv, csvData+'\n', err => {
+    if (err) {
+      console.log('Error writing to csv file', err);
+    } else {
+      console.log(`saved as ${accidentCsv}`);
+    }
   });
 }
 
 /**
- * This function is used to read the weather events from the specified file
- * 
- * @param {string} weatherFile - The path to the weather CSV file 
- * @returns {Promise} - A promise that resolves to an array of weather data objects
+ * Helper function to extract accident data as a CSV string 
+ *  
+ * @param {Object} accident - The accident data.
+ * @returns {string} - The CSV string for accident data.
  * 
  * @author Maara Vanessa Purici
  */
-export async function readWeatherData(weatherFile) {
-  const weatherData = [];
-  await new Promise((resolve, reject) => {
-    fs.createReadStream(weatherFile).
-      pipe(csv()).
-      on('data', (weather) => {
-        weatherData.push(weather);
-      }).
-      on('end', resolve).
-      on('error', reject);
-  });
-  return weatherData;
+function extractAsCsvForAccidents(accident) {
+  // To do this, I used an example form here
+  // https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
+  const rows = accident.map(acc =>
+     `${acc.ID}, ${acc.State}, ${acc.City}, ${acc.Severity}, ${acc.Start_Time}, 
+      ${acc.End_Time}, ${acc.Start_Lat}, ${acc.Start_Lng}, ${acc.Description},
+      ${acc.Street}, ${acc.End_lat}, ${acc.End_Lng}, ${acc['Distance(mi)']},
+      ${acc['Temperature(F)']}`
+  );
+  return rows.join("\n");
 }
 
 /**
- * This function is used to initialize the CSV streams and write headers 
- * if needed
- * 
- * @param {string} accidentCsv - Path to the accident CSV file.
- * @param {string} weatherCsv - Path to the weather CSV file.
- * @returns {Object} - An object containing the accident and weather CSV streams
+ * Function used to add the header to the matching accident
+ * events CSV file, if the file is empty
+ *  
+ * @returns {string} - The CSV string for accident data.
  * 
  * @author Maara Vanessa Purici
  */
-export function initializeAccidentsStream(accidentCsv) {
-  // Setting up streams for CSV Output
-  // flags: 'a' used so that new data is added instead of overiding
-  // learned about flags from here -> https://nodejs.org/api/fs.html#file-system-flags
-  const accidentCsvStream = fs.createWriteStream(accidentCsv, { flags: 'a' });
+export function writeToAccidentsCsvHeader(accidentCsv) {
+  // To do this, I used an example form here
+  // https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
+  // Checking if file exists and is empty
+  if (fs.existsSync(accidentCsv) && fs.statSync(accidentCsv).size > 0) {
+    console.log(`Header already exists in ${accidentCsv}`);
+    return;
+  } else {
+    fs.writeFile(accidentCsv, extractAsCsvForAccidentsHeader(), err => {
+      if (err) {
+        console.log('Error writing the header to the csv file', err);
+      } else {
+        console.log(`Header saved to ${accidentCsv}`);
+      }
+    });
+  }
+}
+
+
+/**
+ * Function used to extract the header for the 
+ * accident data as a CSV string.
+ * 
+ * @returns {string} - The CSV header string for accident data.
+ * 
+ * @author Maara Vanessa Purici
+ */
+function extractAsCsvForAccidentsHeader() {
+  // To do this, I used an example form here
+  // https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
   const accidentColumns = [
     'ID', 'State', 'City', 'Severity', 'Start_Time', 
     'End_Time', 'Start_Lat', 'Start_Lng', 'Description', 
     'Street', 'End_lat', 'End_Lng', 'Distance(mi)', 
     'Temperature(F)'
   ];
-  
-  // Writing headers if the files are empty
-  if (
-    !fs.existsSync(accidentCsv) || 
-    fs.readFile(accidentCsv, 'utf-8').trim() === ''
-  ) {
-    accidentCsvStream.write(accidentColumns.join(',') + '\n');
-  }
-
-  return accidentCsvStream;
+  return accidentColumns.join("\n");
 }
-export function initializeWeatherStream(weatherCsv) {
-  // Setting up streams for CSV Output
-  // flags: 'a' used so that new data is added instead of overiding
-  // learned about flags from here -> https://nodejs.org/api/fs.html#file-system-flags
-  const weatherCsvStream = fs.createWriteStream(weatherCsv, { flags: 'a' });
+
+/**
+ * This function is used to write to the matched weather CSV file
+ *  
+ * @param {string} weatherCsv - Path to the matching weather CSV file.
+ * @param {Object} weather - The weather to add to the CSV file.
+ * 
+ * @author Maara Vanessa Purici
+ */
+export function writeToWeatherCsv(weatherCsv, weather) {
+  // To do this, I used an example form here
+  // https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
+  const csvData = extractAsCsvForWeather(weather);
+  fs.writeFile(weatherCsv, csvData+'\n', err => {
+    if (err) {
+      console.log('Error writing to csv file', err);
+    } else {
+      console.log(`saved as ${weatherCsv}`);
+    }
+  });
+}
+
+/**
+ * Helper function to extract weather data as a CSV string 
+ *  
+ * @param {Object} weather - The weather data.
+ * @returns {string} - The CSV string for weather data.
+ * 
+ * @author Maara Vanessa Purici
+ */
+function extractAsCsvForWeather(weather) {
+  // To do this, I used an example form here
+  // https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
+  const rows = weather.map(wea =>
+     `${wea.EventId}, ${wea.State}, ${wea.City}, ${wea['StartTime(UTC)']},  
+      ${wea['EndTime(UTC)']}, ${wea.Severity}, ${wea.Type},
+      ${wea.LocationLat}, ${wea.LocationLng}, ${wea['Precipitation(in)']}`
+  );
+  return rows.join("\n");
+}
+
+/**
+ * Function used to add the header to the matching weather
+ * events CSV file, if the file is empty
+ *  
+ * @returns {string}- The header for the CSV file.
+ * 
+ * @author Maara Vanessa Purici
+ */
+export function writeToWeatherCsvHeader(weatherCsv) {
+  // To do this, I used an example form here
+  // https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
+  // Checking if file exists and is empty
+  if (fs.existsSync(weatherCsv) && fs.statSync(weatherCsv).size > 0) {
+    console.log(`Header already exists in ${weatherCsv}`);
+    return;
+  } else {
+    fs.writeFile(weatherCsv, extractAsCsvForWeatherHeader(), err => {
+      if (err) {
+        console.log('Error writing the header to the csv file', err);
+      } else {
+        console.log(`Header saved to ${weatherCsv}`);
+      }
+    });
+}
+}
+
+/**
+ * Function used to extract the header for the 
+ * weather data as a CSV string.
+ * 
+ * @returns {string} - The CSV header string for weather data.
+ * 
+ * @author Maara Vanessa Purici
+ */
+function extractAsCsvForWeatherHeader() {
+  // To do this, I used an example form here
+  // https://heynode.com/blog/2020-02/reading-and-writing-csv-files-nodejs/
   const weatherColumns = [
     'EventId', 'State', 'City', 'StartTime(UTC)', 
     'EndTime(UTC)', 'Severity', 'Type', 
     'LocationLat', 'LocationLng', 'Precipitation(in)'
   ];
-
-  // Writing headers if the files are empty
-  if (
-    !fs.existsSync(weatherCsv) || 
-    fs.readFile(weatherCsv, 'utf-8').trim() === ''
-  ) {
-    weatherCsvStream.write(weatherColumns.join(',') + '\n');
-  }
-
-  return weatherCsvStream;
+  return weatherColumns.join("\n");
 }
 
 /**
@@ -143,29 +260,27 @@ export function isDataMatching(accident, weather) {
 
 /**
  * This function is used to add the matching accident and weather data to 
- * the respective JSON and CSV files if they are not already recorded
+ * the respective CSV files if they are not already recorded
  * 
  * @param {Object} accident - The accident data object
  * @param {Object} weather - The weather data object
- * @param accidentCsvStream - The writable stream for the accident CSV file
- * @param weatherCsvStream - The writable stream for the weather CSV file
+ * @param accidentCsv - The file to write to the matching accident eventss
+ * @param weatherCsv - The file to write to the matching weather events
  * 
  * @author Maara Vanessa Purici
  */
-export function addMatchingData(accident, weather, accidentCsvStream, weatherCsvStream) {
+export function addMatchingData(accident, weather, accidentCsv, weatherCsv) {
   if (isDataMatching(accident, weather)){
     // Checking if this match has already been recorded in the CSV files
-    if (!eventExists(accidentCsvStream, accident.ID)) {
+    if (!eventExists(accidentCsv, accident.ID)) {
       // Writing matched accident to CSV
-      const accidentRow = Object.values(accident).join(',') + '\n';
-      accidentCsvStream.write(accidentRow);
+      writeToAccidentsCsv(accidentCsv, accident);
       console.log('Matching accident data saved.');
     }
     
     if (!eventExists(weatherCsvStream, weather.EventId)) {
       // Writing matched weather event to CSV
-      const weatherRow = Object.values(weather).join(',') + '\n';
-      weatherCsvStream.write(weatherRow);
+      writeToWeatherCsv(weatherCsv, weather);
       console.log('Matching weather data saved.');
     }
   }
@@ -174,39 +289,31 @@ export function addMatchingData(accident, weather, accidentCsvStream, weatherCsv
 /**
  * Async function used to match accident event with weather events based on 
  * the State, City, Location Latitude and Logitude and Time 
- * The mathcing records is writen to JSON and CSV files
+ * The mathcing records is writen to the CSV files
  * 
- * @param {string} accidentFile - The path to the accidents CSV file 
- * @param {string} weatherFile - The path to the weather CSV file
+ * @param {string} accidentFile - The path to the accidents CSV file to read from
+ * @param {string} weatherFile - The path to the weather CSV file to read from
+ * @param {string} matchedAccidentCsv - The file to write to the matching accident events
+ * @param {string} matchedWeatherCsv - The file to write to the matching weather events
  * 
  * @author Maara Vanessa Purici
  */
 export async function matchAccidentsWithWeather(accidentFile, weatherFile, matchedAccidentCsv, matchedWeatherCsv) {
-  const weatherData = await readWeatherData(weatherFile);
+  const weatherData = await readCsvData(weatherFile);
+  const accidentData = await readCsvData(accidentFile)
 
-  const accidentCsvStream = initializeAccidentsStream(matchedAccidentCsv);
-  const weatherCsvStream  = initializeWeatherStream(matchedWeatherCsv);
-
-  // Processing accidents and match with weather data
-  await new Promise((resolve, reject) => {
-    // Creating a readable stream that read data from the specified CSV file
-    fs.createReadStream(accidentFile).
-      pipe(csv()).
-      on('data', (accident) => {
-        // This code runs each time data is available to read
-        weatherData.forEach((weather) => {
-          addMatchingData(accident, weather, accidentCsvStream, weatherCsvStream);
-        });
-      }).
-      on('end', () => {
-        // This code runs when all the data has been read
-        accidentCsvStream.end();
-        weatherCsvStream.end();
+  try{
+    writeToAccidentsCsvHeader(matchedAccidentCsv);
+    writeToWeatherCsvHeader(matchedWeatherCsv);
+    accidentData.forEach((accident) => {
+      weatherData.forEach((weather) => {
+        addMatchingData(accident, weather, matchedAccidentCsv, matchedWeatherCsv);
         console.log('Matching accidents and weather data saved.');
-        resolve();
-      }).
-      on('error', reject);
-  });
+      });
+    });
+  } catch (error) {
+    console.error('Error while looking for matching events: ', error);
+  }
 }
 
 /**
