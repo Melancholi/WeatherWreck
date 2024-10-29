d5b4a9042165de5a7fe8144c768f733c36bb2381
Youry Nelson <yourymorgannelson@gmail.com>
2024-10-29 19:28:32 -0400

    Rework accidents tests to a single test suite

--- WeatherWreck/server/test/accidentTests.test.mjs
+++ WeatherWreck/server/test/accidentTests.test.mjs
@@ -105,28 +105,22 @@ const mockListAccidents = [
   }
 ];
 
-const stubDB = sinon.stub(db, 'readAll');
-
-describe('Restoring original function', ()=>{
-  afterEach(()=>{
-    sinon.restore();
-  });
-});
-describe('Mokcing the db', ()=>{
-  beforeEach(()=>{
-    stubDB.resolves(mockListAccidents);
-  });
-});
-
 //Retrives all accidents
 describe('GET /accidents', () => {
+  after(()=>{
+    sinon.restore();
+  });
+  before(()=>{
+    const stubDB = sinon.stub(db, 'readAll');
+    stubDB.resolves(mockListAccidents);
+  });
   it('should retrive all accidents', async()=>{
     const response = await request(app).get('/api/accidents');
-    expect(response.body).to.deep.equal([mockListAccidents]);
+    expect(response.body).to.deep.equal(mockListAccidents);
   });
   it('should respond with status code 200', async () => {
     const response = await request(app).get('/api/accidents');
-    expect(response.statusCode).to.equal(200);
+    expect(response.status).to.equal(200);
   });
 });
 
