f1e1174afc7dde5563c99063f95484051452e55d
Iana Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-10-29 00:28:03 -0400

    Add a simple component to fetch accident data.

---
+++ WeatherWreck/client/src/components/fetchData.jsx
@@ -0,0 +1,34 @@
+import {useState, useEffect} from 'react';
+
+export default function FetchData(){
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/accidents'); 
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  },[]);
+
+  if(loading){
+    return <p> Loading ...</p>
+  }else{
+    return( 
+    <div>
+      <h2> Accidents Data</h2>
+      <ul> {data.map((item, index)=>(
+        <li key={index}> {JSON.stringify(item)}</li>
+      ))}
+      </ul>
+    </div>);
+  } 
+}
