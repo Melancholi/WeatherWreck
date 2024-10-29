7cd6e9a4436d1d9854dbc8b58017be68fba71056
Iana Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-10-28 22:26:26 -0400

    Fix some errors.

---
+++ WeatherWreck/server/test/accidentTests.test.mjs
@@ -0,0 +1,9 @@
+// import * as chai from './chai';
+// import chaiAsPromised from 'chai-as-promised';
+// import * as accidentController from '../controllers/accidentController.mjs';
+// import request from "./supertest";
+
+// const expect = chai.expect;
+
+
+
