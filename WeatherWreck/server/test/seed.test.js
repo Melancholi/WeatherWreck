61e69f9cc1fd79ba138f5b94cb6787539e0c8a07
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-10-29 22:16:58 -0400

    Made 1 test pass

--- WeatherWreck/server/test/seed.test.js
+++ WeatherWreck/server/test/seed.test.js
@@ -1,17 +1,16 @@
-import { db } from '../db/db.js';
 import { getFilePaths, formatFile } from '../util/seed.js';
 import * as fs from 'fs/promises';
 import * as chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { MongoClient } from 'mongodb';
 import sinon from 'sinon';
+import path from 'path';
 
 const expect = chai.expect;
 chai.use(chaiAsPromised);
 
-const folderPath = path.join('./db', '/mockData');
-
 describe('Database seed tests', function() {
+  const folderPath = path.join('./db', '/mockData');
+
   describe('Testing the getFilePaths method', function() {
     let readdirStub;
     before(() => {
@@ -76,11 +75,11 @@ describe('Database seed tests', function() {
       const formattedData = formatFile(mockWeather, 'WeatherForecast')
 
       expect(formattedData).to.deep.include({
-        'EventId': 'W-01', 'State':'CO', 'City': 'Saguache',
+        'EventId': 'W-01', 'State':'QC', 'City': 'Montreal',
+        'StartTime(UTC)': '12:34:00', 'EndTime(UTC)': '15:54:00',
+        'Severity': 'Light', 'Type': 'Snow',
+        'Precipitation(in)': 0.0,
         'Date': '2022-01-01',
-        'StartTime(UTC)': '12:34:00',
-        'EndTime(UTC)': '15:54:00',
-        'Precipitation': 0.0
       });
       expect(formattedData).to.not.have.property('LocationLng');
       expect(formattedData).to.not.have.property('LocationLat');
