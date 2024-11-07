6979103338831375a89eecd1faf8dfc63913fe07
Youry Nelson <yourymorgannelson@gmail.com>
2024-11-07 00:02:32 -0500

    add route to test matching algorithm

--- WeatherWreck/server/api.mjs
+++ WeatherWreck/server/api.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import accidentRouter from './routers/accident.mjs';
 import weatherRouter from './routers/weather.mjs';
-
+import { db } from './db/db.mjs';
 //Create APP
 const app = express();
 
@@ -13,5 +13,16 @@ app.use('/api/accidents', accidentRouter);
 
 // Route for weather-related API endpoints
 app.use('/api/weather', weatherRouter);
+
+app.get('/test/:state', async (req, res, next) =>{
+  try{
+    const state = req.params.state.toUpperCase();
+    const data = await db.readByConditionMatch('CarAccidents', 'WeatherForecast',
+      { State: {$eq : state} });
+    res.json(data);
+  }catch(error){
+    next(res.status(500).json({error : error.message}));
+  }
+});
 
 export default app;
