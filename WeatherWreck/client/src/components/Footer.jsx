2f3e608542b900e96f96c8f598a7c4b152654415
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-07 20:37:37 -0500

    Add basic header and footer

---
+++ WeatherWreck/client/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="teammates">
+        <p>Iana</p>
+        <p>Youry</p>
+        <p>Maara</p>
+      </div>
+      
+      <div className="accidentsAttribution">
+        <p>https://arxiv.org/abs/1902.06792</p>
+      </div>
+
+      <div className="weatherAttribution">
+        <p>https://arxiv.org/abs/1906.05409</p>
+        <p>https://arxiv.org/abs/1909.09638</p>
+      </div>
+    </footer>
+  );
+}
