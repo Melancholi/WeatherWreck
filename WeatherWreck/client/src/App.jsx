310c243b8f2f36510addf5a5f41457fb8285fd31
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-09 16:11:04 -0500

    Made change in css

--- WeatherWreck/client/src/App.jsx
+++ WeatherWreck/client/src/App.jsx
@@ -1,12 +1,12 @@
 import FetchData from './components/fetchData.jsx';
-import Header from './components/Header.jsx';
+import NavBar from './components/NavBar.jsx';
 import Footer from './components/Footer.jsx';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header />
+      <NavBar />
       <FetchData/>
       <Footer />
     </div>
--- WeatherWreck/client/src/components/NavBar.jsx
+++ WeatherWreck/client/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 
-export default function Header() {
+export default function NavBar() {
   return (
     <header>
       <div>
@@ -8,7 +8,7 @@ export default function Header() {
 
       <div id="navBar">
         <nav>
-          <ul>
+          <ul id="navUl">
             {/* TO DO: Make the the folowing clickable and functional */}
             <li>View Accidents</li>
             <li>View Charts</li>
