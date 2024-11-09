f9a544baa11ffe846e441c6c2411d30a743837bf
Youry Nelson <yourymorgannelson@gmail.com>
2024-11-09 11:30:08 -0500

    Fix formatting issues with reading rows of csv

--- WeatherWreck/server/util/seed.js
+++ WeatherWreck/server/util/seed.js
@@ -23,24 +23,33 @@ export async function getFilePaths(folderPath) {
  * formats depending on the type
  */
 export function formatFile(row, type){
+  //encoding issues with the file, id had has a weirdly enconded
+  //  character that makes it unable to access
+  //  I just decided to do the simple 
+  // option to get the id
+  const idKey = Object.keys(row)[0];
   let formatData;
   if(type.includes('CarAccidents')){
-    const startPoint = [row.Start_Lng, row.Start_Lat];
-    const endPoint = [row.End_Lng, row.End_lat];
+    //encoding issues with the file, id had has a weirdly enconded
+    //  character that makes it unable to access
+    //  I just decided to do the simple 
+    // option to get the id
+    row['Start_Point'] = [row.Start_Lng, row.Start_Lat];
+    row['End_Point'] = [row.End_Lng, row.End_Lat];
     row['Date'] = row.Start_Time.split(' ')[0];
     row['Start_Time'] = row.Start_Time.split(' ')[1];
     //fix a small issue due to time being 00:00:00
     row['End_Time'] = row['End_Time'].split(' ')[1] === '00:00:00'
       ? '23:59:59' : row['End_Time'].split(' ')[1];
+    row['Accident_Key'] = row[idKey];
+    delete row[idKey];
     //remove now useless rows
     delete row['Start_Lat'];
     delete row['Start_Lng'];
-    delete row['End_lat'];
+    delete row['End_Lat'];
     delete row['End_Lng'];
     formatData = {  
       ...row,
-      'Start_Point': startPoint,
-      'End_Point': endPoint,
     };
   }else{
     //remove now useless rows
@@ -51,6 +60,8 @@ export function formatFile(row, type){
     //fix a small issue due to time being 00:00:00
     row['EndTime(UTC)'] = row['EndTime(UTC)'].split(' ')[1] === '00:00:00'
       ? '23:59:59' : row['EndTime(UTC)'].split(' ')[1];
+    row['Weather_Key'] = row[idKey];
+    delete row[idKey];
     formatData = {
       ...row,
     };
