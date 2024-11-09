b355de4521fcb835b18f64bddda824bd8d9ed1db
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-09 16:22:15 -0500

    Made changes to the css

--- WeatherWreck/client/src/components/Footer.jsx
+++ WeatherWreck/client/src/components/Footer.jsx
@@ -3,18 +3,20 @@ export default function Footer() {
   return (
     <footer>
       <div className="teammates">
-        <p>Iana</p>
-        <p>Youry</p>
-        <p>Maara</p>
+        <p>Iana Feniuc</p>
+        <p>Youry Nelson</p>
+        <p>Maara Purici</p>
       </div>
       
       <div className="accidentsAttribution">
-        <p>https://arxiv.org/abs/1902.06792</p>
+        <p>Accidents Dataset Attribution: </p>
+        <a href="https://arxiv.org/abs/1902.06792" target="_blank">Short and Long-term Pattern Discovery Over Large-Scale Geo-Spatiotemporal Data</a>
       </div>
 
       <div className="weatherAttribution">
-        <p>https://arxiv.org/abs/1906.05409</p>
-        <p>https://arxiv.org/abs/1909.09638</p>
+        <p>Weather Dataset Attributions: </p>
+        <a href="https://arxiv.org/abs/1906.05409" target="_blank">A Countrywide Traffic Accident Dataset</a>
+        <a href="https://arxiv.org/abs/1909.09638" target="_blank">Accident Risk Prediction based on Heterogeneous Sparse Data: New Dataset and Insights</a>
       </div>
     </footer>
   );
