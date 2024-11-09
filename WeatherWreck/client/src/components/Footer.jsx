af816358df8fb3744b55b28af813289dcc93985b
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-09 17:27:56 -0500

    Fix eslint errors

--- WeatherWreck/client/src/components/Footer.jsx
+++ WeatherWreck/client/src/components/Footer.jsx
@@ -10,13 +10,19 @@ export default function Footer() {
       
       <div className="accidentsAttribution">
         <p>Accidents Dataset Attribution: </p>
-        <a href="https://arxiv.org/abs/1902.06792" target="_blank">Short and Long-term Pattern Discovery Over Large-Scale Geo-Spatiotemporal Data</a>
+        <a href="https://arxiv.org/abs/1902.06792" target="_blank">
+          Short and Long-term Pattern Discovery Over Large-Scale Geo-Spatiotemporal Data
+        </a>
       </div>
 
       <div className="weatherAttribution">
         <p>Weather Dataset Attributions: </p>
-        <a href="https://arxiv.org/abs/1906.05409" target="_blank">A Countrywide Traffic Accident Dataset</a>
-        <a href="https://arxiv.org/abs/1909.09638" target="_blank">Accident Risk Prediction based on Heterogeneous Sparse Data: New Dataset and Insights</a>
+        <a href="https://arxiv.org/abs/1906.05409" target="_blank">
+          A Countrywide Traffic Accident Dataset
+        </a>
+        <a href="https://arxiv.org/abs/1909.09638" target="_blank">
+          Accident Risk Prediction based on Heterogeneous Sparse Data: New Dataset and Insights
+        </a>
       </div>
     </footer>
   );
