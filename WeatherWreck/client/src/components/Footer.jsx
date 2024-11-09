2b1abedd87b8b2869de27485c8092d12b94bbc00
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-09 18:37:45 -0500

    Change the size of attribution

--- WeatherWreck/client/src/components/Footer.jsx
+++ WeatherWreck/client/src/components/Footer.jsx
@@ -5,14 +5,15 @@ export default function Footer() {
       <section id="dataInfo">
         <section id="accidenInfo">
           <div className="accidentsAttribution">
-            <p>Data Taken From:</p>
-            <a href="https://www.kaggle.com/datasets/sobhanmoosavi/us-accidents"  target="_blank">
+            <p className="info">Data Taken From:</p>
+            <a href="https://www.kaggle.com/datasets/sobhanmoosavi/us-accidents"  target="_blank"
+              className="info">
               US Accidents (2016 - 2023) made by sobhanmoosavi on kaggle
             </a>
           </div>
           <div className="accidentsAttribution">
-            <p>Accidents Dataset Attribution: </p>
-            <a href="https://arxiv.org/abs/1902.06792" target="_blank">
+            <p className="info">Accidents Dataset Attribution: </p>
+            <a href="https://arxiv.org/abs/1902.06792" target="_blank" className="info">
               Short and Long-term Pattern Discovery Over Large-Scale Geo-Spatiotemporal Data
             </a>
           </div>
@@ -22,18 +23,18 @@ export default function Footer() {
 
         <section id="weatherInfo">
           <div className="weatherAttribution">
-            <p>Data Taken From:</p>
+            <p className="info">Data Taken From:</p>
             <a href="https://www.kaggle.com/datasets/sobhanmoosavi/us-weather-events" 
-              target="_blank">
+              target="_blank" className="info">
               US Weather Events (2016 - 2022) made by sobhanmoosavi on kaggle
             </a>
           </div>
           <div className="weatherAttribution">
-            <p>Weather Dataset Attributions: </p>
-            <a href="https://arxiv.org/abs/1906.05409" target="_blank">
+            <p className="info">Weather Dataset Attributions: </p>
+            <a href="https://arxiv.org/abs/1906.05409" target="_blank" className="info">
               A Countrywide Traffic Accident Dataset
             </a>
-            <a href="https://arxiv.org/abs/1909.09638" target="_blank">
+            <a href="https://arxiv.org/abs/1909.09638" target="_blank" className="info">
               Accident Risk Prediction based on Heterogeneous Sparse Data: New Dataset and Insights
             </a>
           </div>
