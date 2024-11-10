8011e25deba63f2b1fc2bcca708c46f01bc19adf
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-10 00:02:36 -0500

    Add basic elements for ChartsPage

--- WeatherWreck/client/src/components/ChartsPage.jsx
+++ WeatherWreck/client/src/components/ChartsPage.jsx
@@ -1,8 +1,48 @@
+import {useState} from 'react';
+import BarChart from './BarChart.jsx';
+import PieChart from './PieChart.jsx';
 
 export default function ChartsPage() {
+  const [checked, setChecked] = useState('bar')
+  function handleCheckedMode(mode) {
+    setChecked(mode);
+  }
+
   return (
-    <div>
-      <h1>Temporary Charts Page</h1>
+    <div id="chartComponent">
+      <section id="dataPanel">
+        <div>
+          <input 
+            type="checkbox"
+            id="barChart"
+            value="bar"
+            checked={checked === 'bar'}
+            onChange={() => handleCheckedMode('bar')}/>
+          <label>View Bar Chart</label>
+        </div>
+
+            
+        <div>
+          <input 
+            type="checkbox"
+            id="pieChart"
+            value="pie"
+            checked={checked === 'pie'}
+            onChange={() => handleCheckedMode('pie')}/>
+          <label>View Pie Chart</label>
+        </div>    
+      </section>
+      
+      <section id="dataCharts">
+        {checked === 'bar' &&
+          <BarChart />
+        }
+
+        
+        {checked === 'pie' &&
+          <PieChart />
+        }
+      </section>
     </div>
   );
 }
