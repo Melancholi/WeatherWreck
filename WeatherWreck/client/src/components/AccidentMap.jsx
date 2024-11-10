53bbad3a0a2e0a68ab12502959fec321124580e4
Iana Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-11-10 01:09:50 -0500

    Add a map component + map.css.

--- WeatherWreck/client/src/components/AccidentMap.jsx
+++ WeatherWreck/client/src/components/AccidentMap.jsx
@@ -2,19 +2,45 @@ import FetchData from './fetchData.jsx';
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 import { Icon  } from 'leaflet';
+import { useState, useEffect} from 'react';
 import { 
   MapContainer, 
   TileLayer, 
   Marker,
-  Popup,
-  Polyline
+  Popup
 } from 'react-leaflet';
 
 export default function AccidentMap() {
+  const attribution = 
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+  const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  const [error, setError] = useState(null);
+
   return (
-    <div id="map">
-      <h1>Temporary Accident Map Page</h1>
-      <FetchData/>
+    <div className="ui-container">
+      <div className ="ui-controls">
+      </div>
+      {/* Error message display */}
+      {error && <div>{error}</div>}
+
+      {/* See leaflet-container CSS class */}
+      <MapContainer
+        center={[39.8283, -98.5795]}
+        zoom={4}
+        zoomControl={true}
+        updateWhenZooming={false}
+        updateWhenIdle={true}
+        preferCanvas={true}
+        minZoom={3}
+        maxZoom={16}
+      >
+        <TileLayer
+          attribution={attribution}
+          url={tileUrl}
+        />    
+        
+      </MapContainer>
+      
     </div>
   );
 }
