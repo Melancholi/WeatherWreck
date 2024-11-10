f89a25a4ed83172f1be4692d909588b0b0ad4759
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-10 11:41:12 -0500

    Made temporary pie chart

--- WeatherWreck/client/src/components/PieChart.jsx
+++ WeatherWreck/client/src/components/PieChart.jsx
@@ -1,8 +1,26 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
 
 export default function PieChart() {
+  const data = [{
+    values: [19, 26, 55],
+    labels: ['Residential', 'Non-Residential', 'Utility'],
+    type: 'pie'
+  }];
+  
+  var layout = {
+    height: 400,
+    width: 500,
+    title: "Test Pie Chart"
+  };
+
   return (
     <div>
       <h1>Temporary PieChart Display</h1>
+      <Plot
+        data={data}
+        layout={layout}
+      />
     </div>
   );
 }
