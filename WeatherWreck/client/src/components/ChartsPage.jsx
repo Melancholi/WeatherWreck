1fca9e54377882e4dbf8f124deaaac89d0020df8
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-11 17:55:09 -0500

    Made progress for the fetching

--- WeatherWreck/client/src/components/ChartsPage.jsx
+++ WeatherWreck/client/src/components/ChartsPage.jsx
@@ -1,8 +1,40 @@
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 import BarChart from './BarChart.jsx';
 import PieChart from './PieChart.jsx';
 
+const validWeatherType = [
+  'Cold', 'Fog', 'Hail', 'Precipitation',
+  'Rain', 'Snow', 'Storm'
+];
+
+// const accidents = {
+//   'Cold': [],
+//   'Fog': [],
+//   'Hail': [],
+//   'Precipitation': [],
+//   'Rain': [],
+//   'Snow': [],
+//   'Storm': []
+// };
+
 export default function ChartsPage() {
+  const [acc, setAcc] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect (() => {
+    const data = async () => {
+      try {
+        const response = await fetch('/api/accidents/matched'); 
+        const result = await response.json();
+        setAcc(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    
+    data();
+  }, []);
   const [checked, setChecked] = useState('bar');
   function handleCheckedMode(mode) {
     setChecked(mode);
@@ -32,16 +64,25 @@ export default function ChartsPage() {
           <label>View Pie Chart</label>
         </div>    
       </section>
-      
+
+      {loading ? (
+        <>
+          <p>Loading...</p>
+        </>
+      ): (
+        <>
+          <p></p>
+        </>
+      )}
       <section id="dataCharts">
         {checked === 'bar' &&
-          <BarChart />
-        }
+            <BarChart />
+          }
 
-        
-        {checked === 'pie' &&
-          <PieChart />
-        }
+          
+          {checked === 'pie' &&
+            <PieChart />
+          }
       </section>
     </div>
   );
