53e6910eb6df4b59dd7b1b396cb4b1701e08ca28
Youry Nelson <yourymorgannelson@gmail.com>
2024-11-12 01:23:16 -0500

    Add loops to structure the data chart

--- WeatherWreck/client/src/components/PieChart.jsx
+++ WeatherWreck/client/src/components/PieChart.jsx
@@ -11,9 +11,34 @@ import Plot from 'react-plotly.js';
  */
 export default function PieChart({weatherOfAccidents}) {
   /*
-    Example from https://plotly.com/javascript/pie-charts/
-    I used the example for the "Basic Pie Chart"
+    Example from https://plotly.com/javascript/sunburst-charts/
   */
+  const values = [];
+  const labels = [];
+  const parents = [];
+  Object.entries(weatherOfAccidents).forEach(([weatherCondition, accidents])=>{
+    const severity = {'1':0, '2':0, '3':0, '4':0};
+
+    //First set the quantities for the severity
+    accidents.forEach(accident =>{
+      severity[accident.Accident_Severity]++;
+    });
+
+    //sets up the hierarchy
+    labels.push(weatherCondition);
+    parents.push('');
+    values.push(accidents.length);
+    //Add the values to display
+    Object.entries(severity).forEach(([severity, count]) => {
+      if (count > 0) {
+        labels.push(`Severity ${severity}`);
+        parents.push(weatherCondition);
+        values.push(count);
+      }
+    });
+
+  });
+
   const data = [{
     values: [19, 26, 55],
     labels: ['Residential', 'Non-Residential', 'Utility'],
