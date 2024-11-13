2d42520f4492ec0c15c5cf2aae6240f78c453af3
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-12 22:43:23 -0500

    Start rebase

--- WeatherWreck/client/src/components/BarChart.jsx
+++ WeatherWreck/client/src/components/BarChart.jsx
@@ -71,7 +71,7 @@ export default function BarChart({ events, validWeatherType }) {
 
   return( 
     <figure>
-      <h1>Accident Percent by Weather Condition</h1>
+      <h1 className="chartsH1">Accident Percent by Weather Condition</h1>
       <Plot
         data={data}
         layout={layout}
