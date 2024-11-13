4a0800ceb2eb981cc5b843c91dc1c50719cedca0
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-12 19:09:18 -0500

    Fix conflicts

--- WeatherWreck/client/src/components/BarChart.jsx
+++ WeatherWreck/client/src/components/BarChart.jsx
@@ -1,43 +1,13 @@
 import Plot from 'react-plotly.js';
-import { useEffect, useState } from 'react';
-
 const validWeatherType = [
   'Cold', 'Fog', 'Hail', 'Precipitation',
   'Rain', 'Snow', 'Storm'
 ];
-// const accidents = [
-//   cold = [],
-//   fog = [],
-//   hail = [],
-//   precipitation = [],
-//   rain = [],
-//   snow = [],
-//   storm = []
-// ];
 
 /**
  * Displays accident percentages based on weather condition.
  */
 export default function BarChart() {
-  const [acc, setAcc] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect (() => {
-    const d = async () => {
-      try {
-        const response = await fetch('/api/accidents/matched'); 
-        const result = await response.json();
-        setAcc(result);
-        //acc.push(result);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    // Call the fetch function when component mounts
-    d();
-  }, []);
-
   /*
     Example from https://plotly.com/javascript/bar-charts/
     I used the example for the "Basic Bar Chart"
