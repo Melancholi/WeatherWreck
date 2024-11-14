e19693534f59a6721c09903300c2a50965cac4fd
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-14 09:51:29 -0800

    Try to fix nav bar

--- WeatherWreck/client/src/App.jsx
+++ WeatherWreck/client/src/App.jsx
@@ -9,22 +9,39 @@ function App() {
     Learned how to do this from this youtube video 
     https://youtu.be/SLfhMt5OUPI?t=355&si=8nmXMeosxHSO2kUY
   */
-  let component;
-  switch (window.location.pathname) {
-  case '/':
-    component = <AccidentMap />;
-    break;
-  case '/charts':
-    component = <ChartsPage />;
-    break;
-  default:
-    component = <AccidentMap />;
-    break;
-  }
+  // let component = <AccidentMap />;
+  // const mapView = document.getElementById('map');
+  // const chartsView = document.getElementById('charts');
+  // mapView.addEventListener('click', () => {
+  //   component = <AccidentMap />;
+  // });
+  // chartsView.addEventListener('click', () => {
+  //   component = <ChartsPage />;
+  // });
+  
+  // mapView.addEventListener('click', () => {
+  //   component = <AccidentMap />;
+  // });
+  // chartsView.addEventListener('click', () => {
+  //   component = <ChartsPage />;
+  // });
+
+  // switch (window.location.pathname) {
+  // case '/':
+  //   component = <AccidentMap />;
+  //   break;
+  // case '/charts':
+  //   component = <ChartsPage />;
+  //   break;
+  // default:
+  //   component = <AccidentMap />;
+  //   break;
+  // }
+  
   return (
     <div>
       <NavBar />
-      {component}
+      {/* {component} */}
       <Footer />
     </div>
   );
