a2068868fe05dcebe51e3c7116697bd5109fa062
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-14 09:51:29 -0800

    Try to fix nav bar

--- WeatherWreck/client/src/components/NavBar.jsx
+++ WeatherWreck/client/src/components/NavBar.jsx
@@ -1,20 +1,52 @@
+import ChartsPage from './ChartsPage.jsx';
+import AccidentMap from './AccidentMap.jsx';
 
+function Foo() {
+  document.getElementById("map").onclick = AccidentMap;
+}
+
+function Boo() {
+  document.getElementById("charts").onclick = ChartsPage;
+}
 export default function NavBar() {
+  let component = <AccidentMap />;
+  const mapView = document.getElementById('#map');
+  const chartsView = document.getElementById('#charts');
+  // mapView.addEventListener('click', () => {
+  //   component = <AccidentMap />;
+  // });
+  // chartsView.addEventListener('click', () => {
+  //   component = <ChartsPage />;
+  // });
+  function Foo() {
+    mapView.onclick = AccidentMap;
+    component = <AccidentMap />;
+  }
+  
+  function Boo() {
+    chartsView.onclick = ChartsPage;
+    component = <ChartsPage />;
+  }
   return (
-    <header>
-      <div id="appTitle">
-        <img src="../../public/favicon.ico" alt="App Logo" id="logo"/>
-        <h1> WeatherWreck</h1>
-      </div>
+    <>
+      <header>
+        <div id="appTitle">
+          <img src="../../public/favicon.ico" alt="App Logo" id="logo"/>
+          <h1> WeatherWreck</h1>
+        </div>
 
-      <div id="navBar">
-        <nav>
-          <ul id="navUl">
-            <a href="/" className="nav-link">View Accidents</a>
-            <a href="/charts" className="nav-link">View Charts</a>
-          </ul>
-        </nav>
-      </div>
-    </header>
+        <div id="navBar">
+          <nav>
+            <ul id="navUl">
+              {/* <a className="nav-link" id="map">View Accidents</a> */}
+              <button id="map" onclick="Foo">View Accidents</button>
+              {/* <a className="nav-link" id="charts">View Charts</a> */}
+              <button id="charts" onclick="Boo">View Charts</button>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      {component}
+    </>
   );
 }
