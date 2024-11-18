f07d1c97d67b3713b70a524e19a034d2eb32aef8
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-18 11:45:22 -0800

    Test fixing navbar

--- WeatherWreck/client/src/components/NavBar.jsx
+++ WeatherWreck/client/src/components/NavBar.jsx
@@ -1,52 +1,27 @@
 import ChartsPage from './ChartsPage.jsx';
 import AccidentMap from './AccidentMap.jsx';
 
-function Foo() {
-  document.getElementById("map").onclick = AccidentMap;
-}
-
-function Boo() {
-  document.getElementById("charts").onclick = ChartsPage;
-}
-export default function NavBar() {
-  let component = <AccidentMap />;
-  const mapView = document.getElementById('#map');
-  const chartsView = document.getElementById('#charts');
-  // mapView.addEventListener('click', () => {
-  //   component = <AccidentMap />;
-  // });
-  // chartsView.addEventListener('click', () => {
-  //   component = <ChartsPage />;
-  // });
-  function Foo() {
-    mapView.onclick = AccidentMap;
-    component = <AccidentMap />;
-  }
-  
-  function Boo() {
-    chartsView.onclick = ChartsPage;
-    component = <ChartsPage />;
-  }
+export default function NavBar({ setCurrentPage }) {
   return (
-    <>
-      <header>
-        <div id="appTitle">
-          <img src="../../public/favicon.ico" alt="App Logo" id="logo"/>
-          <h1> WeatherWreck</h1>
-        </div>
+    <header>
+      <div id="appTitle">
+        <img src="../../public/favicon.ico" alt="App Logo" id="logo" />
+        <h1>WeatherWreck</h1>
+      </div>
 
-        <div id="navBar">
-          <nav>
-            <ul id="navUl">
-              {/* <a className="nav-link" id="map">View Accidents</a> */}
-              <button id="map" onclick={Foo}>View Accidents</button>
-              {/* <a className="nav-link" id="charts">View Charts</a> */}
-              <button id="charts" onclick={Boo}>View Charts</button>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      {component}
-    </>
+      <div id="navBar">
+        <nav>
+          <ul id="navUl">
+            {/* Use onClick to switch pages */}
+            <li className="nav-link" onClick={() => setCurrentPage('AccidentMap')}>
+              View Accidents
+            </li>
+            <li className="nav-link" onClick={() => setCurrentPage('ChartsPage')}>
+              View Charts
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
