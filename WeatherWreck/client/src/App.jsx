a313874fea0170cee7fb61743c5151b2d3d1b22e
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-18 11:45:22 -0800

    Test fixing navbar

--- WeatherWreck/client/src/App.jsx
+++ WeatherWreck/client/src/App.jsx
@@ -3,45 +3,27 @@ import AccidentMap from './components/AccidentMap.jsx';
 import NavBar from './components/NavBar.jsx';
 import Footer from './components/Footer.jsx';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  /* 
-    Learned how to do this from this youtube video 
-    https://youtu.be/SLfhMt5OUPI?t=355&si=8nmXMeosxHSO2kUY
-  */
-  // let component = <AccidentMap />;
-  // const mapView = document.getElementById('map');
-  // const chartsView = document.getElementById('charts');
-  // mapView.addEventListener('click', () => {
-  //   component = <AccidentMap />;
-  // });
-  // chartsView.addEventListener('click', () => {
-  //   component = <ChartsPage />;
-  // });
-  
-  // mapView.addEventListener('click', () => {
-  //   component = <AccidentMap />;
-  // });
-  // chartsView.addEventListener('click', () => {
-  //   component = <ChartsPage />;
-  // });
+  const [currentPage, setCurrentPage] = useState('AccidentMap');
 
-  // switch (window.location.pathname) {
-  // case '/':
-  //   component = <AccidentMap />;
-  //   break;
-  // case '/charts':
-  //   component = <ChartsPage />;
-  //   break;
-  // default:
-  //   component = <AccidentMap />;
-  //   break;
-  // }
-  
+  // Function to render the selected component
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'AccidentMap':
+        return <AccidentMap />;
+      case 'ChartsPage':
+        return <ChartsPage />;
+      default:
+        return <AccidentMap />;
+    }
+  };
+
   return (
     <div>
-      <NavBar />
-      {/* {component} */}
+      <NavBar setCurrentPage={setCurrentPage} />
+      {renderPage()}
       <Footer />
     </div>
   );
