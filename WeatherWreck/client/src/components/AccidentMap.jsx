cfea33b3a600637cfdfd159809986126a50b6e46
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-23 12:44:36 -0500

    Fix height

--- WeatherWreck/client/src/components/AccidentMap.jsx
+++ WeatherWreck/client/src/components/AccidentMap.jsx
@@ -132,8 +132,8 @@ export default function AccidentMap() {
       </div>);
   }else{
     return (
-      <div id="ui-contrlos">
-        <div id="ui-container">
+      <div id="ui-container">
+        <div id="ui-contrlos">
           {/* Error message display */}
           <div id="filters">
             {error && <div id="error-message">{error}</div>}
