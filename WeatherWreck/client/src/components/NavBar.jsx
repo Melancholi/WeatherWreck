29955bf09856c138dad8538c5761ac6a8994e907
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-23 13:03:04 -0500

    Update nav buttons

--- WeatherWreck/client/src/components/NavBar.jsx
+++ WeatherWreck/client/src/components/NavBar.jsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import favicon from '../../public/favicon.ico';
 import './NavBar.css';
+
 export default function NavBar({ setCurrentPage }) {
+  // Tracking the current active page
+  const [activePage, setActivePage] = useState('AboutUs');
+
+  // Handling navigation clicks
+  const handleNavClick = (page) => {
+    // Setting the active page
+    setActivePage(page);
+    // Setting the current page
+    setCurrentPage(page);
+  };
+
   return (
     <header>
       <div id="appTitle">
@@ -11,14 +24,26 @@ export default function NavBar({ setCurrentPage }) {
       <div id="navBar">
         <nav>
           <ul id="navUl">
-            {/* Use onClick to switch pages */}
-            <li className="nav-link" onClick={() => setCurrentPage('AboutUs')}>
+            <li
+              className={`nav-link ${activePage === 'AboutUs' ? 'active' : ''}`}
+              onClick={() => handleNavClick('AboutUs')}
+            >
               About Us
             </li>
-            <li className="nav-link" onClick={() => setCurrentPage('AccidentMap')}>
+            <li
+              className={`nav-link ${
+                activePage === 'AccidentMap' ? 'active' : ''
+              }`}
+              onClick={() => handleNavClick('AccidentMap')}
+            >
               View Accidents Map
             </li>
-            <li className="nav-link" onClick={() => setCurrentPage('ChartsPage')}>
+            <li
+              className={`nav-link ${
+                activePage === 'ChartsPage' ? 'active' : ''
+              }`}
+              onClick={() => handleNavClick('ChartsPage')}
+            >
               View Charts
             </li>
           </ul>
