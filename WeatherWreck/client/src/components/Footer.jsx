d8db9b6e2bbc3e28ea096fbe64f2f6a4435a5efa
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-22 20:31:09 -0500

    Add content to component

--- WeatherWreck/client/src/components/Footer.jsx
+++ WeatherWreck/client/src/components/Footer.jsx
@@ -5,13 +5,6 @@ export default function Footer() {
       <section id="dataInfo">
         <section id="accidenInfo">
           <div className="accidentsAttribution">
-            <p className="info">Data Taken From:</p>
-            <a href="https://www.kaggle.com/datasets/sobhanmoosavi/us-accidents"  target="_blank"
-              className="info">
-              US Accidents (2016 - 2023) made by sobhanmoosavi on kaggle
-            </a>
-          </div>
-          <div className="accidentsAttribution">
             <p className="info">Accidents Dataset Attribution: </p>
             <a href="https://arxiv.org/abs/1902.06792" target="_blank" className="info">
               Short and Long-term Pattern Discovery Over Large-Scale Geo-Spatiotemporal Data
@@ -20,13 +13,6 @@ export default function Footer() {
         </section> 
         
         <section id="weatherInfo">
-          <div className="weatherAttribution">
-            <p className="info">Data Taken From:</p>
-            <a href="https://www.kaggle.com/datasets/sobhanmoosavi/us-weather-events" 
-              target="_blank" className="info">
-              US Weather Events (2016 - 2022) made by sobhanmoosavi on kaggle
-            </a>
-          </div>
           <div className="weatherAttribution">
             <p className="info">Weather Dataset Attributions: </p>
             <a href="https://arxiv.org/abs/1906.05409" target="_blank" className="info">
