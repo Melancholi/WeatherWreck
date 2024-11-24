6a7799ab2e14a902b82d0352102bcf309e8e69a3
Youry Nelson <yourymorgannelson@gmail.com>
2024-11-24 16:33:55 -0500

    Revert changes to lazy load bar chart,

--- WeatherWreck/client/src/components/ChartsPage.jsx
+++ WeatherWreck/client/src/components/ChartsPage.jsx
@@ -1,15 +1,11 @@
 import { useEffect, useState, lazy, Suspense } from 'react';
 import bobLoadingImage from '../assets/bob.webp';
 import './ChartsPage.css';
-
+import BarChart from './BarChart.jsx';
 //Lazy imports, allows the app to not have to install all of the views
 const PieChart = lazy(
   () => import('./PieChart.jsx')
 );
-const BarChart = lazy(
-  () =>  import('./BarChart.jsx')
-);
-
 /**
  * List of valid weather conditions for filtering accident data.
  * @constant {string[]}
@@ -132,12 +128,10 @@ export default function ChartsPage() {
         : 
         <section id="dataCharts">
           {checked === 'bar' &&
-              <Suspense fallback={<div>Loading Bar Chart...</div>}>
                 <BarChart 
                   events={acc}
                   validWeatherType={validWeatherType}
                 />
-              </Suspense>
           }
 
           {checked === 'pie' && 
