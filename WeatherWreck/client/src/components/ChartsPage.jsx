ed6222b2ebc0ea591f6af0951401c44fe2380f32
MaaraVP <maara.purici@dawsoncollege.qc.ca>
2024-11-25 11:48:52 -0800

    Fix rebase conflicts

--- WeatherWreck/client/src/components/ChartsPage.jsx
+++ WeatherWreck/client/src/components/ChartsPage.jsx
@@ -4,9 +4,6 @@ import BarChart from './BarChart.jsx';
 import './ChartsPage.css';
 
 //Lazy imports, allows the app to not have to install all of the views
-const BarChart = lazy(
-  () => import('./BarChart.jsx')
-);
 const PieChart = lazy(
   () => import('./PieChart.jsx')
 );
