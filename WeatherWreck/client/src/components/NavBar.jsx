16bb40f5a261126bdd7f15cf7ef226c4be7ab863
Iana Feniuc <iana.feniuc@dawsoncollege.qc.ca>
2024-11-26 16:53:39 -0500

    Add jsdocs + comments to the NavBar file.

--- WeatherWreck/client/src/components/NavBar.jsx
+++ WeatherWreck/client/src/components/NavBar.jsx
@@ -1,8 +1,24 @@
 import favicon from './../assets/favicon.ico';
 import './NavBar.css';
 
+/**
+ * NavBar Component
+ * 
+ * Renders the application's navigation bar with links to different pages. The component
+ * highlights the active page and allows users to switch between "About Us," "Accident Map,"
+ * and "Charts Page."
+ * 
+ * @component
+ * @param {Object} props - The component props.
+ * @param {string} props.currentPage - The current active page.
+ * @param {function} props.setCurrentPage - Function to update the current page state.
+ * @returns {JSX.Element} The navigation bar component.
+ */
 export default function NavBar({ currentPage, setCurrentPage }) {
   return (
+    /**
+     * Main header element containing the app title and navigation bar.
+     */
     <header>
       <div id="appTitle">
         <img src={favicon} alt="App Logo" id="logo" />
