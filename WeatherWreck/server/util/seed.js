df63e7e378118b688507be1703cc1a924e544b10
Youry Nelson <yourymorgannelson@gmail.com>
2025-02-20 14:38:54 -0500

    Added data and adjustment to limit data added to DB

--- WeatherWreck/server/util/seed.js
+++ WeatherWreck/server/util/seed.js
@@ -5,7 +5,7 @@ import { parse } from 'csv-parse';
 import { db } from '../db/db.mjs';
 import { getFilePaths, formatFile} from './utils.js';
 
-const folderPath = path.join('./db', '/mockData');
+const folderPath = path.join('./data');
 const dbName = 'WeatherWreck';
 const collections = ['CarAccidents', 'WeatherForecast'];
 
@@ -25,18 +25,37 @@ const seed = async () =>{
     await Promise.all(data.map(async(collection)=>{
       const dataToInsert = [];
       const fileContent = await fs.readFile(collection['filePath'], {encoding: 'utf-8'});
+      let rowsAdded = 0;
       //this csv-parse was based of onlines docs 
       await new Promise((resolve, reject) => {
-        parse(fileContent, { 
+        const parser = parse(fileContent, { 
           columns: true,
-        }).
+        });
+        parser.
           on('data', (row) => {
+            if(rowsAdded >= 1000){
+              console.log('close');
+              parser.destroy();
+              resolve();
+              return;
+            }
             //format the data before sending
             const formatData = formatFile(row, collection['name']);
             dataToInsert.push(formatData);
+            rowsAdded++;
           }).
-          on('end', resolve).
-          on('error', reject); 
+          on('end', () => {
+            console.log(`Finished parsing ${rowsAdded} rows.`);
+            resolve();
+          }).
+          on('error', (err) => {
+            console.error('Error while parsing:', err);
+            reject(err);
+          }).
+          on('close', ()=>{
+            console.log('closed?');
+            resolve();
+          });
       });
       await db.createMany(collection['name'], dataToInsert);
     }));
